13597e783c87701d615a27a49a259add8aa72e0f
Clarksj4 <clarksj4@student.op.ac.nz>
2017-04-22 19:31:13 +1200

    Add test heap count property

--- FibonacciTest/FibonacciHeapTests.cs
+++ FibonacciTest/FibonacciHeapTests.cs
@@ -21,6 +21,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node1);
             Assert.AreEqual(heap.Minimum.Priority, 3);
             Assert.AreEqual(heap.Minimum.Value, 'a');
+            Assert.AreEqual(heap.Count, 4);
 
             // Insert new minimum
             HeapNode<float, char> node5 = heap.Insert(2, 'e');
@@ -29,6 +30,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node5);
             Assert.AreEqual(heap.Minimum.Priority, 2);
             Assert.AreEqual(heap.Minimum.Value, 'e');
+            Assert.AreEqual(heap.Count, 5);
         }
 
         [TestMethod()]
@@ -47,6 +49,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node1);
             Assert.AreEqual(heap.Minimum.Priority, 3);
             Assert.AreEqual(heap.Minimum.Value, 'a');
+            Assert.AreEqual(heap.Count, 4);
 
             // Remove minimum element
             heap.DeleteMin();
@@ -55,6 +58,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node2);
             Assert.AreEqual(heap.Minimum.Priority, 7);
             Assert.AreEqual(heap.Minimum.Value, 'b');
+            Assert.AreEqual(heap.Count, 3);
         }
 
         [TestMethod()]
@@ -76,6 +80,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum.Priority, 1);
             Assert.AreEqual(heap.Minimum.Value, 'd');
             Assert.AreEqual(heap.Minimum, node4);
+            Assert.AreEqual(heap.Count, 4);
 
             // Delete min so trees are consolidated
             heap.DeleteMin();
@@ -87,6 +92,7 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node2);
             Assert.AreEqual(heap.Minimum.Priority, 0);
             Assert.AreEqual(heap.Minimum.Value, 'b');
+            Assert.AreEqual(heap.Count, 3);
         }
 
         [TestMethod()]
@@ -95,11 +101,17 @@ namespace FibonacciHeap.Tests
             // Heap
             FibonacciHeap<float, char> heap = new FibonacciHeap<float, char>();
 
+            // Check heap starts with no nodes
+            Assert.AreEqual(heap.Count, 0);
+
             // Add collection of nodes
             HeapNode<float, char> node2 = heap.Insert(7, 'b');
             HeapNode<float, char> node1 = heap.Insert(3, 'a');
             HeapNode<float, char> node3 = heap.Insert(14, 'c');
             HeapNode<float, char> node4 = heap.Insert(35, 'd');
+
+            // Check heap has correct number of nodes
+            Assert.AreEqual(heap.Count, 4);
 
             char minimum = heap.Pop();
 
@@ -111,23 +123,28 @@ namespace FibonacciHeap.Tests
             Assert.AreEqual(heap.Minimum, node2);
             Assert.AreEqual(heap.Minimum.Priority, 7);
             Assert.AreEqual(heap.Minimum.Value, 'b');
+            Assert.AreEqual(heap.Count, 3);
+
 
             minimum = heap.Pop();
 
             // Check returned item is the correct value, and is not still in heap
             Assert.AreEqual(minimum, 'b');
             Assert.AreNotEqual(heap.Minimum.Value, minimum);
+            Assert.AreEqual(heap.Count, 2);
 
             minimum = heap.Pop();
 
             // Check returned item is the correct value, and is not still in heap
             Assert.AreEqual(minimum, 'c');
             Assert.AreNotEqual(heap.Minimum.Value, minimum);
+            Assert.AreEqual(heap.Count, 1);
 
             minimum = heap.Pop();
 
             // Check returned item is the correct value
             Assert.AreEqual(minimum, 'd');
+            Assert.AreEqual(heap.Count, 0);
         }
     }
 }
